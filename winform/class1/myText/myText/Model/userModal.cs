cf67f704ff25260d7007b1e08c99ba8c80bd9be8
ycfeng666 <1073225787@qq.com>
2021-09-24 10:42:50 +0800

    Initial commit

---
+++ winform/class1/myText/myText/Model/userModal.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myText.Modal
+{
+    class userModal
+    {
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        private string Username = "张三";
+        public string userNmae{ 
+            get {
+                return this.Username;
+            }
+            set { this.Username = value; }
+        } 
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        private int Age = 24;
+        public int age
+        {
+            get {
+                return this.Age;
+            }
+            set { this.Age = value; }
+        } 
+
+        /// <summary>
+        /// // 性别
+        /// </summary>
+        private string Sex = "男";
+        public string sex
+        {
+            get {
+                return this.Sex;
+            }           
+        } 
+
+        /// <summary>
+        /// // 号码
+        /// </summary>
+        private string Phone = "12345678910";
+        public string phone
+        {
+            get {
+                return this.Phone;
+            }            
+        }
+        /// <summary>
+        /// 工资
+        /// </summary>
+        private int MonthMoney = 0;
+        public int monthMoney { 
+            get { 
+                return this.MonthMoney; 
+            } set { 
+                this.MonthMoney = value; 
+            } 
+        }
+
+        /// <summary>
+        /// 手机品牌
+        /// </summary>
+        private string PhoneName = "华为";
+        public string phoneName
+        {
+            get {
+                return this.PhoneName;
+            }
+        }
+        /// <summary>
+        /// 手机型号
+        /// </summary>
+        private string PhoneType = "P30";
+        public string phoneType
+        {
+            get {
+                return this.PhoneType;
+            }
+        }
+
+        /// <summary>
+        /// // 手机颜色
+        /// </summary>
+        private string PhoneColor = "白色";
+        public string phoneColor
+        {
+            get {
+                return this.PhoneColor;
+            }
+        }
+    }
+}
---
+++ winform/class1/myText/myText/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using myText.Modal;
+
+namespace myText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            userModal useM = new userModal();
+            Console.Write("请输入姓名:");
+            useM.userNmae = Console.ReadLine();
+            Console.Write("请输入年龄:");
+            // 尝试强制转换
+            useM.age = int.TryParse(Console.ReadLine(), out int intAge)? intAge : useM.age;
+            Console.Write("请输入月薪:");
+            useM.monthMoney = int.TryParse(Console.ReadLine(), out int intMoney)? intMoney : useM.monthMoney;
+            Console.WriteLine(string.Format("姓名：{0}，年龄：{1}，性别：{2}\n" +
+                "我的手机号码：{3}，我的手机名称是：{4}\n" +
+                "我的手机型号：{5}，我的手机颜色是：{6}", 
+                useM.userNmae, useM.age, useM.sex, 
+                useM.phone,useM.phoneName, 
+                useM.phoneType, useM.phoneColor));
+            Console.ReadKey();
+        }
+    }
+}
