a908c6102a058406bab2192ebb542dd98d262cbb
ycfeng666 <1073225787@qq.com>
2021-09-29 14:01:06 +0800

    class2

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # 课程记录
-## winForm第一次课
+## winForm
+### 第一次课
 2021年9月22日 周三
-**课程要求：**
-1. 使用c#控制台应用程序输出姓名,年龄,性别,手机号码,
-2. 使用c#控制台应用程序输出手机名称,手机型号,手机品牌
-3. 使用c#控制台应用程序读取并输出姓名,年龄,月薪
+[https://github.com/ycfeng666/hsClass/tree/main/winform/class1/myText](https://github.com/ycfeng666/hsClass/tree/main/winform/class1/myText)
+
+**实验目的：**
+1. 熟练掌握C#开发环境的使用。
+1. 熟悉开发环境，学会编写控制台程序。
+1. 熟练掌握数据的输入输出。
+
+**实验重难点**
+1. 开发环境
+1. C#的语句，会使用控制台进行输入输出
+
+**实验内容**
+1. 定义4个变量，分别表示：姓名、性别、年龄，电话，然后输出，类似于第2题的格式。
+1. 在屏幕上输出：我的手机品牌是华为，型号是P30，颜色是白色。
+1. 定义姓名、年龄、工资，要求从键盘输入，然后输出，注意：Console.Write和Console.WriteLine的区别。学会Console.Readline()和Console.Read()的使用
+
+### 第二次课
+2021年9月27日 周一
+[https://github.com/ycfeng666/hsClass/tree/main/winform/class2/numSort](https://github.com/ycfeng666/hsClass/tree/main/winform/class2/numSort)
+**实验内容**
+1. 使用控制台进行输入三个整数，输出倒数排序（使用2种方法）
---
+++ winform/class2/numSort/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace numSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入第一个数：");
+            int num1 = int.TryParse(Console.ReadLine(), out int isIntNum1) ? isIntNum1 : 0;
+            Console.Write("请输入第二个数：");
+            int num2 = int.TryParse(Console.ReadLine(), out int isIntNum2) ? isIntNum2 : 1;
+            Console.Write("请输入第三个数：");
+            int num3 = int.TryParse(Console.ReadLine(), out int isIntNum3) ? isIntNum3 : 2;
+
+            // 方法1
+            int maxNum = (num1 > num2 ? num1 : num2) > num3 ? (num1 > num2 ? num1 : num2) : num3;
+            int minNum = (num1 < num2 ? num1 : num2) < num3 ? (num1 < num2 ? num1 : num2) : num3;
+            int midNum = (num1 + num2 + num3) - maxNum - minNum;
+            Console.WriteLine("方法一：");
+            Console.WriteLine("最小的是："+ minNum + ",第二个是：" + midNum+",最大的是：" + maxNum);
+
+            int[] arr = { num1, num2, num3 };
+
+            //方法二:冒泡排序
+            Console.WriteLine("方法二：");
+            int[] arrNum1 = bubbleSort(arr);
+            Console.WriteLine("最小的是：" + arrNum1[0] + ",第二个是：" + arrNum1[1] + ",最大的是：" + arrNum1[2]);
+
+            //方法三:插入排序
+            Console.WriteLine("方法三：");
+            int[] arrNum2 = insertSort(arr);
+            Console.WriteLine("最小的是：" + arrNum2[0] + ",第二个是：" + arrNum2[1] + ",最大的是：" + arrNum2[2]);
+
+            //方法三:插入简单选择排序
+            Console.WriteLine("方法四：");
+            int[] arrNum3 = selectSort(arr);
+            Console.WriteLine("最小的是：" + arrNum3[0] + ",第二个是：" + arrNum3[1] + ",最大的是：" + arrNum3[2]);
+
+            //方法三:快速排序
+            Console.WriteLine("方法五：");
+            int[] arrNum4 = quickSort(arr,0,arr.Length-1);
+            Console.WriteLine("最小的是：" + arrNum4[0] + ",第二个是：" + arrNum4[1] + ",最大的是：" + arrNum4[2]);
+
+            Console.ReadKey();
+        }
+        // 方法二，冒泡排序
+        public static int[] bubbleSort(int[] arrNum) {
+            for (int i = 0; i < arrNum.Length-1; i++)
+            {
+                for (int j = 0; j < arrNum.Length-1-i; j++)
+                {
+                    if (arrNum[j] > arrNum[j+1]) {
+                        int tempNum = arrNum[j + 1];
+                        arrNum[j + 1] = arrNum[j];
+                        arrNum[j] = tempNum;
+                    }
+                }
+            }
+            return arrNum;
+        }
+        // 方法三，插入排序
+        public static int[] insertSort(int[] arrNum) {
+            int preIndex, current;
+            for (int i = 1; i < arrNum.Length; i++)
+            {
+                preIndex = i - 1;
+                current = arrNum[i];
+                // 拿到i的位置的元素跟前面的元素做比较
+                while (preIndex>=0 && arrNum[preIndex]>current) {
+                    // 向后移动
+                    arrNum[preIndex + 1] = arrNum[preIndex];
+                    preIndex --;
+                }
+                arrNum[preIndex + 1] = current;
+            }
+            return arrNum;
+        }
+
+        // 方法四，简单选择排序
+        public static int[] selectSort(int[] arrNum) {
+            for (int i = 0; i < arrNum.Length-1; i++)
+            {
+                // 记录最小索引
+                int minIndex = i;
+                for (int j = i + 1; j < arrNum.Length; j++)
+                {
+                    if (arrNum[j] < arrNum[minIndex]) {
+                        minIndex = j;
+                    }
+                }
+
+                // 交互最小值,如果索引是最小值就不用交换
+                if (minIndex !=i) {
+                    int tempNum = arrNum[i];
+                    arrNum[i] = arrNum[minIndex];
+                    arrNum[minIndex] = tempNum;
+                }
+            }
+            return arrNum;
+        }
+
+        // 方法五，快速排序
+        public static int[] quickSort(int[] arrNum, int left ,int right) {
+            if (left<right) {
+                // 基准数，把比它的或等于它的放在它的左边，大于它的放在它的右边
+                int x = arrNum[left];
+                int i = left;
+                int j = right;
+                // i==j，找到了一个中间的位置，这个中间数是基准数所在的位置
+                while (i < j) {
+                    // 从后往前比较
+                    // 找到一个比较基数，小于或等于的数字，应该把它放在x的左边
+                    while (i < j) {
+                        if (arrNum[j] <= x)
+                        {
+                            arrNum[i] = arrNum[j];
+                            break;
+                        }
+                        else {
+                            j--;
+                        }
+                    }
+                    while (i<j) {
+                        if (arrNum[j] > x)
+                        {
+                            arrNum[j] = arrNum[i];
+                        }
+                        else {
+                            i++;
+                        }
+                    }
+                }
+                arrNum[i] = x;
+                quickSort(arrNum, left, i - 1);
+                quickSort(arrNum, i+1, right);
+            }
+            return arrNum;
+        }
+    }
+}
