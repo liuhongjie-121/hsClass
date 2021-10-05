39159556031c78f95b52d47107fbe91acb9f94dd
ycfeng666 <1073225787@qq.com>
2021-10-05 13:57:52 +0800

    10.5修改

--- winform/class1/myText/myText/Model/userModal.cs
+++ winform/class1/myText/myText/Model/userModal.cs
@@ -4,95 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace myText.Modal
+namespace myText.UserModal
 {
     class userModal
     {
         /// <summary>
         /// 姓名
         /// </summary>
-        private string Username = "张三";
-        public string userNmae{ 
-            get {
-                return this.Username;
-            }
-            set { this.Username = value; }
-        } 
+        public string userNmae { get; set; }
         /// <summary>
         /// 年龄
         /// </summary>
-        private int Age = 24;
-        public int age
-        {
-            get {
-                return this.Age;
-            }
-            set { this.Age = value; }
-        } 
-
+        public int age { get; set; }
         /// <summary>
         /// // 性别
         /// </summary>
-        private string Sex = "男";
-        public string sex
-        {
-            get {
-                return this.Sex;
-            }           
-        } 
-
+        public string sex { get; set; }
         /// <summary>
         /// // 号码
         /// </summary>
-        private string Phone = "12345678910";
-        public string phone
-        {
-            get {
-                return this.Phone;
-            }            
-        }
+        public string phone { get; set; }
         /// <summary>
         /// 工资
         /// </summary>
-        private int MonthMoney = 0;
-        public int monthMoney { 
-            get { 
-                return this.MonthMoney; 
-            } set { 
-                this.MonthMoney = value; 
-            } 
-        }
-
+        public int monthMoney { get; set; }
         /// <summary>
         /// 手机品牌
         /// </summary>
-        private string PhoneName = "华为";
-        public string phoneName
-        {
-            get {
-                return this.PhoneName;
-            }
-        }
+        public string phoneName { get; set; }
         /// <summary>
         /// 手机型号
         /// </summary>
-        private string PhoneType = "P30";
-        public string phoneType
-        {
-            get {
-                return this.PhoneType;
-            }
-        }
-
+        public string phoneType { get; set; }
         /// <summary>
         /// // 手机颜色
         /// </summary>
-        private string PhoneColor = "白色";
-        public string phoneColor
-        {
-            get {
-                return this.PhoneColor;
-            }
-        }
+        public string phoneColor { get; set; }
     }
 }
--- winform/class1/myText/myText/Program.cs
+++ winform/class1/myText/myText/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using myText.Modal;
+using myText.UserModal;
 
 namespace myText
 {
@@ -12,6 +12,13 @@ namespace myText
         static void Main(string[] args)
         {
             userModal useM = new userModal();
+
+            // 设定默认值
+            useM.sex = "男";
+            useM.phoneName = "华为手机";
+            useM.phoneType = "P30";
+            useM.phoneColor = "白色";
+
             Console.Write("请输入姓名:");
             useM.userNmae = Console.ReadLine();
             Console.Write("请输入年龄:");
@@ -22,7 +29,9 @@ namespace myText
             Console.WriteLine(string.Format("姓名：{0}，年龄：{1}，性别：{2}\n" +
                 "我的手机号码：{3}，我的手机名称是：{4}\n" +
                 "我的手机型号：{5}，我的手机颜色是：{6}", 
-                useM.userNmae, useM.age, useM.sex, 
+                useM.userNmae == "" ? "吉良吉影": useM.userNmae, // 非空判断
+                useM.age == 0 ? 33: useM.age, // 非空判断
+                useM.sex , 
                 useM.phone,useM.phoneName, 
                 useM.phoneType, useM.phoneColor));
             Console.ReadKey();
