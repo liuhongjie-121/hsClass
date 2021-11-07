98345677f929138ee091b98704436e7d6bddd176
ycfeng666 <1073225787@qq.com>
2021-11-07 21:59:45 +0800

    调整doc

---
+++ docs/static/js/chunk-2d22d746.ce3c95cb.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,t,a){"use strict";a.r(t);var c=a("7a23"),e={class:"about"},s=Object(c["m"])("h1",null,"This is an about page",-1),u=[s];function o(n,t){return Object(c["E"])(),Object(c["l"])("div",e,u)}var b=a("6b0d"),d=a.n(b);const i={},r=d()(i,[["render",o]]);t["default"]=r}}]);
